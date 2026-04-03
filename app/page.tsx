dca965222635b9599586cd34487a762f8d829850
Luciana-Cuenca <luciana.cuenca@campusucc.edu.co>
2026-04-02 21:23:33 -0500

    agregar secciones skills y experience

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import SeparatorTriangle from "../components/SeparatorTriangle";
 import SectionDivider from "../components/SectionDivider";
 import Skills from "@/components/Skills";
 import Proyects from "@/components/Proyects";
+import Experience from "@/components/Experience";
 
 export default function Home() {
   return (
@@ -25,6 +26,10 @@ export default function Home() {
       <SectionDivider />
 
       <Proyects />
+
+      <SectionDivider />
+
+      <Experience />
 
     </main>
   );
