4e54d48cb3c6b506a47edd9b47db2c7c55022f34
Luciana-Cuenca <luciana.cuenca@campusucc.edu.co>
2026-04-12 22:38:35 -0500

    cambios en el sectionDivider y el triangulo

--- components/SectionDivider.tsx
+++ components/SectionDivider.tsx
@@ -1,20 +1,17 @@
 export default function SectionDivider() {
   return (
-    <div className="w-full bg-white flex items-center justify-center px-6 md:px-10 py-1">
+    <div className="section-divider w-full bg-white px-4 md:px-8 py-1">
+      <div className="w-full max-w-[1400px] mx-auto flex items-center justify-center">
+        <div className="flex-1 h-[3px] bg-[#EC0202] mx-4"></div>
 
-      {/* línea izquierda */}
-      <div className="flex-1 h-[3px] bg-[#EC0202] mx-4"></div>
+        <img
+          src="/icon.svg"
+          alt="icon"
+          className="w-7 h-7 md:w-8 md:h-8 object-contain"
+        />
 
-      {/* icono centro */}
-      <img
-        src="/icon.svg"
-        alt="icon"
-        className="w-7 h-7 md:w-8 md:h-8 object-contain"
-      />
-
-      {/* línea derecha */}
-      <div className="flex-1 h-[3px] bg-[#EC0202] mx-4"></div>
-
+        <div className="flex-1 h-[3px] bg-[#EC0202] mx-4"></div>
+      </div>
     </div>
   );
 }
